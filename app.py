1e214ba5b979a3c192c87a7a8e7c914bf3d14978
pimpalikarprathamaiml23-lang <pimpalikarpratham.aiml23@sbjit.edu.in>
2026-03-21 12:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.express as px  # This is the line that was crashing
 from textblob import TextBlob
 
 # --- PAGE CONFIG ---
